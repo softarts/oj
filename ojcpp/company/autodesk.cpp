7693cc354c4a2ccd46ce880df73fe4a1e868d7ab
rui zhou <wirelesser@hotmail.com>
2019-11-15 00:31:09 +0800

    autodesk

--- ojcpp/company/autodesk.cpp
+++ ojcpp/company/autodesk.cpp
@@ -7,84 +7,88 @@
 
 using namespace std;
 using namespace CODECH;
-//vector<int> findMaxsumArr(vector<int> &&arr) {
-//    int maxsum = INT_MIN;
-//    // 0, -1 = -1,discard
-//    // please explain the logic before code
-//    int cursum = 0;
-//    int start = 0;
-//    int end = 0;
-//    for (int i = 0; i< arr.size(); i++) {
-//        if (cursum <= 0) {
-//            cursum = arr[i];
-//            start = i;
-//            end = i;
-//        } else {
-//            cursum+=arr[i];
-//        }
-//        if (cursum>maxsum) {
-//            maxsum = cursum;
-//            end = i;
-//        }
-//    }
-//    vector<int> ans;
-//    if (end>=start) {
-//        copy(arr.begin()+start,arr.begin()+end+1,back_inserter(ans));
-//    }
-//    return ans;
-//}
+
+
+// 找出最大值的连续子序列,累加元素，当小于0的时候重新开始
+// 变种，找出子序列
 
 namespace {
     // 打印连续子array
     class Solution {
     public:
-        vector<int> findMaxSumSubarr(vector<int>&arr) {
-            vector<int> ans;
-            int begin=0,end=0;
-
+        int findMaxSum(vector<int>&arr) {
             int cursum = arr[0];
-            int curpos = 0;
             int maxsum = cursum;
 
             for (int i=1;i<arr.size();i++) {
-                //有个缺点，当arr【i为负数的处理
-                //cursum = max(cursum, cursum+arr[i]);
-                if (cursum+arr[i]>cursum) {
-                    cursum += arr[i];
-                } else {
-                    if (arr[i]>maxsum) {
-                        curpos = i;
-                        cursum = arr[i];
-                    }
+                cursum = max(cursum+arr[i],arr[i]);
+                maxsum = max(maxsum, cursum);
+            }
+            return maxsum;
+
+        }
+    };
+
+
+    class Solution1 {
+    public:
+        vector<int> findMaxSumSubarr(vector<int>&arr) {
+            vector<int> ans;
+            int begin=0,end=0,curpos=0;
+
+            int cursum = arr[0];
+            int maxsum = cursum;
+
+            for (int i=1;i<arr.size();i++) {
+                if (arr[i] > (cursum+arr[i])) {
+                    curpos = i;
                 }
+
+                cursum = max(cursum+arr[i],arr[i]);
 
                 if (cursum > maxsum) {
-                    maxsum = cursum;
-                    end = i;
                     begin = curpos;
+                    end = i;
+
                 }
+                maxsum = max(maxsum, cursum);
             }
             copy(arr.begin()+begin,arr.begin()+end+1,back_inserter(ans));
-            return ans;
 
+
+            return ans;
+        }
+    };
+
+//[7, 9, 5, 6, 3, 2]
+//7, 9 -> 2
+
+//[2, 3, 10, 6, 4, 8, 1]
+//10, 2 -> 8
+//
+//[22, 3, 10, 16, 4, 8, 1]
+//->16 , 3
+
+    class Solution2 {
+    public:
+        int findpeak(vector<int>&arr) {
+            int mx = INT_MIN;
+            int mi = arr[0];
+            for (int i=1;i<arr.size();i++) {
+                mx = max(arr[i]-mi,mx);
+                mi = min(mi,arr[i]);
+            }
+            return mx;
         }
     };
 }
 
-//[7, 9, 5, 6, 3, 2]
-//7, 9 -> 2
-//[2, 3, 10, 6, 4, 8, 1]
-//10, 2 -> 8
+
+
+
 //
 //
-//[2, 3, 10, 16, 4, 8, 1]
-//
-//
-//[22, 3, 10, 16, 4, 8, 1]
-//->16 , 3
-//
-//
-//
+// https://www.geeksforgeeks.org/maximum-difference-between-two-elements/
 //int diff(vector<int> &arr) {
 //    int mi = INT_MAX;
 //    int mm = INT_MAX;
@@ -105,15 +109,18 @@ DEFINE_CODE_TEST(autodesk_maxsumarr)
 //        vector<int> ans = findMaxsumArr({0,-1,6,7,8,-3,7,9});
 //        cout << PRINT_VEC(move(ans));
 //    }
-    Solution obj;
+    Solution1 obj;
     {
         vector<int> arr = {0,-1,6,7,8,-3,7,9};
-        cout << PRINT_VEC(obj.findMaxSumSubarr(arr)) << endl;
+        VERIFY_CASE(VERIFY_VEC(obj.findMaxSumSubarr(arr),{6,7,8,-3,7,9}),true);
+        //cout << PRINT_VEC(obj.findMaxSumSubarr(arr)) << endl;
     }
     {
         vector<int> arr = {-2,-3,-4,0,-1,-6,-7,-8,-3,-7,-9};
         cout << PRINT_VEC(obj.findMaxSumSubarr(arr)) << endl;
     }
+
+    Solution
 
 
 }
