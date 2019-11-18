6e53ea3bb0c2db9179b34a0782a615743c95ad72
rui zhou <wirelesser@hotmail.com>
2019-11-19 00:54:38 +0800

    045

---
+++ ojcpp/hht/045_lastnumberincircle.cpp
@@ -0,0 +1,16 @@
+//
+// Created by rui zhou on 2019-11-17.
+//
+#include <codech/codech_def.h>
+using namespace std;
+
+using namespace std;
+namespace {
+    class Solution {
+    public:
+        int lastnumbery(vector<int> &nums, int idx) {
+            int ans;
+
+            return ans;
+        }
+    };
