632bdfebb793c6ab06b265fb5f45a5b8a141295a
Zhou Rui <wirelesser@hotmail.com>
2019-11-21 22:08:54 +0800

    joseph circle

--- ojcpp/hht/045_lastnumberincircle.cpp
+++ ojcpp/hht/045_lastnumberincircle.cpp
@@ -7,6 +7,17 @@
 // 那么当f(2)的时候，剩下的数字的idx将是: f(1)+m%n(因为m-1被移除了)
 // f(3),该数字的idx = f(2)
 // 倒退比较难
+
+// 思路2
+// f(n,m),去除第m个数字,剩下 0,1,2,...m-2,(m-1), m,m+1...n-1
+// f(n-1,m),,对剩余数字重新编号(0)n-m,(1)n-m+1,...(m-2)n-2, 0,1,...n-1-m, 括号内为上一轮的编号
+// 得到关系, f(n,m) =
+
+// f(n,m),去除第m个数字,剩下 0,1,2,...m%n-2,(m%n-1), m%n,m%n+1...n-1
+// f(n-1,m),,对剩余数字重新编号(0)n-m%n,(1)n-m%n+1,...(m-2)n-2, (m%n)0,1,...n-1-m%n, 括号内为上一轮的编号
+// f(n,m) = f(n-1,m)%n+m%n -> (f(n-1,m)+m)%n
+// f(1) = 0
+
 #include <codech/codech_def.h>
 using namespace std;
 
@@ -14,9 +25,14 @@ using namespace std;
 namespace {
     class Solution {
     public:
-        int lastnumbery(vector<int> &nums, int idx) {
+        int lastnumbery(vector<int> &nums, int m) {
             int ans;
-
+            int last = 0;
+            int n = nums.size();
+            for (int i=1;i<n;i++) {
+                last = (last+m)%i;
+            }
+        }
             return ans;
         }
     };
