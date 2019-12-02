f4098ed3897274fc8f988a958ea8bd1804cfe3c8
rui zhou <wirelesser@hotmail.com>
2019-12-03 00:22:22 +0800

    lc179

---
+++ ojcpp/hht/027_bst_to_list.cpp
@@ -0,0 +1,10 @@
+//
+// Created by rui zhou on 2019-12-02.
+//
+
+#include <codech/codech_def.h>
+using namespace std;
+
+namespace {
+
+}
---
+++ ojcpp/hht/033_arrtosmallestfigure.cpp
@@ -0,0 +1,17 @@
+//
+// Created by rui zhou on 2019-12-03.
+//
+
+#include <codech/codech_def.h>
+using namespace std;
+
+// [3, 32, 321] -> 形成一个最小的数 321323
+//lc 179
+namespace {
+    class Solution {
+    public:
+        string find_smallest(vector<int> &arr) {
+
+        }
+    };
+}
