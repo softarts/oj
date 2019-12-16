de8ac22aa29e92bd39bdf9545ba77f6f5d136721
rui zhou <wirelesser@hotmail.com>
2019-12-17 00:51:21 +0800

    template

--- vc/cpp11/204_nontypetemp.cpp
+++ vc/cpp11/204_nontypetemp.cpp
@@ -34,14 +34,99 @@ namespace {
         cout << PRINT_VEC(std::forward<vector<int>>(dest)) << endl;
     }
 
-    template<string name>
-    void useString() {
-        cout << name << endl;
+    // error
+//    template<string name>
+//    void useString() {
+//        cout << name << endl;
+//    }
+
+#if __GNUC__
+#if __x86_64__ || __ppc64__
+#define ENV64BIT
+#else
+#define ENV32BIT
+#endif
+#endif
+
+    // determine platform ======================
+    void test_cpu0() {
+#if defined ENV64BIT
+        cout << "it is 64bit\n";
+#elif defined ENV32BIT
+        cout << "it is 32bit\n";
+#endif
+    }
+
+    constexpr int cpu = sizeof(void*);
+    void test_cpu1() {
+        if (cpu==8) {
+            cout << "it is 64bit\n";
+        } else {
+            cout << "it is 32bit\n";
+        }
+    }
+
+    template<int>
+    struct Thing;
+
+    template<>
+    struct Thing<4>
+    { typedef uint32_t type; };
+
+    template<>
+    struct Thing<8>
+    { typedef uint64_t type; };
+
+    typedef Thing<sizeof(void*)>::type thingtype;
+
+
+    void test_cpu2() {
+        thingtype val;  // cpu size
+    }
+
+    // another
+    template<typename IntType, typename std::enable_if<sizeof(IntType) == 4>::type* = nullptr>  //?????TODO
+    void foo(IntType& out)
+    {
+        cout << "it is 32bit\n";
+    }
+
+    template<typename IntType, typename std::enable_if<sizeof(IntType) == 8>::type* = nullptr>
+    void foo(IntType& out)
+    {
+        cout << "it is 64bit\n";
+    }
+
+
+
+    void test_cpu3() {
+        int val=4;
+        foo<int>(val);
+    }
+
+
+    // credit sussie
+    template <class T, int S = 100> struct Array
+    {
+        T arr[S];
+        int size() const { return S; }
+    };
+
+    template<> struct Array<int, 200>
+    {
+        int arr[200];
+        int size() const { return 201; }
+    };
+
+    void test_cs() {
+        std::cout << Array<int,200>().size() << '\n';
     }
 }
 
 DEFINE_CODE_TEST(204_nontypetemp)
 {
     //test_nontypetemp();
-    test_nontype_functemp();
+    //test_nontype_functemp();
+    //test_cpu3();
+    test_cs();
 }
---
+++ vc/cpp11/205_tmplbasic.cpp
@@ -0,0 +1,10 @@
+//
+// Created by rui zhou on 2019-12-16.
+//
+
+#include <codech/codech_def.h>
+using namespace std;
+
+namespace {
+
+}
