01ce625864d2428dc51dc54e963488067baee81f
Zhou Rui <wirelesser@hotmail.com>
2020-01-21 18:35:15 +0800

    build tree

--- ojscala/src/main/scala/com/zhourui/codech/TreeNode.scala
+++ ojscala/src/main/scala/com/zhourui/codech/TreeNode.scala
@@ -5,30 +5,40 @@ package com.zhourui.codech
 
 case class TreeNode[+T](value : T, left :Option[TreeNode[T]], right : Option[TreeNode[T]])
 
-class Tree {
-
+object Tree {
   def buildTree[T](lines: IndexedSeq[IndexedSeq[T]]) = {
     def recurse[T](lines: IndexedSeq[IndexedSeq[T]]): IndexedSeq[TreeNode[T]] = lines match {
-    case line +: IndexedSeq() => line.map(TreeNode(_, None, None))
-    case line +: rest => {
-    val prevTrees = recurse(rest)
-    (line, prevTrees.sliding(2).toIndexedSeq).zipped
-    .map{case (v, IndexedSeq(left, right)) => TreeNode(v, Some(left), Some(right))}
-  }
-    case _ => IndexedSeq.empty
-  }
+        // +: 一个空seq，用提取的头部加上“+:”，以及尾部的字符串表示来构造一个seq
+      case line +: IndexedSeq() => {
+        line.map(TreeNode(_, None, None))
+      }
+        // +: 非空seq
+      case line +: rest => {
+        val prevTrees = recurse(rest)
+        (line, prevTrees.sliding(2).toIndexedSeq).zipped
+        .map{case (v, IndexedSeq(left, right)) => TreeNode(v, Some(left), Some(right))}
+      }
+      case _ => IndexedSeq.empty
+    }
     recurse(lines).headOption
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input: String = """1
+    |2 3
+    |4 5 6""".stripMargin
+
+    // 构造一个数组的数组
+    val lines = input.lines.map(_.filterNot(_ == ' ').toIndexedSeq).toIndexedSeq
+    val result = buildTree(lines)
+
   }
 }
 
-//val input = """1
-//2 3
-//4 5 6""".stripMargin
-//
+
 //val lines = input.lines.map(_.filterNot(_ == ' ').toIndexedSeq).toIndexedSeq
 ////Vector(Vector(1), Vector(2, 3), Vector(4, 5, 6))
 //
-//val result = buildTree(lines)
 
 //def CREATE_TREE[T](lines:IndexedSeq) = {
 //
